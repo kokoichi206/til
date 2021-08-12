e10027c19b37aa2b2657d3cbb8de089c84d7c93a
kokoichi <tt.300607@gmail.com>
2021-08-13 05:29:40 +0900

    sec 1

---
+++ books/java_self/Main.java
@@ -0,0 +1,10 @@
+package books.java_self;
+
+class Main {
+    public static void main(String[] args) {
+        String msg = "";
+        msg += "Hello ";
+        msg += "World!";
+        System.out.println(msg);
+    }
+}
---
+++ books/java_self/chap01/Hello.java
@@ -0,0 +1,10 @@
+package books.java_self.chap01;
+
+import java.time.LocalDateTime;
+
+public class Hello {
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println(time);
+    }
+}
