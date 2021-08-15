66e38e571efc4d1d5d090d02ee5842dc613f61df
kokoichi <tt.300607@gmail.com>
2021-08-16 06:03:06 +0900

    flygame

---
+++ android/FlyGame/app/src/main/java/io/kokoichi/sample/flygame/Flight.java
@@ -0,0 +1,2 @@
+package io.kokoichi.sample.flygame;public class Flight {
+}
---
+++ android/android_text/FlyGame/app/src/main/java/io/kokoichi/sample/flygame/GameActivity.kt
@@ -0,0 +1,10 @@
+package io.kokoichi.sample.flygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
