9a7f0a963e416f4db0d4f6be0d9ae0d16cbed410
kokoichi206 <tt.300607@gmail.com>
2021-09-05 16:45:48 +0900

    feat: wallet 情報を表示させる

--- block_chain_go/wallet/wallet.go
+++ block_chain_go/wallet/wallet.go
@@ -78,6 +78,19 @@ func (w *Wallet) BlockchainAddress() string {
 	return w.blockchainAddress
 }
 
+// クライアントに返すための json 準備
+func (w *Wallet) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		PrivateKey        string `json:"private_key"`
+		PublicKey         string `json:"public_key"`
+		BlockchainAddress string `json:"blockchain_address"`
+	}{
+		PrivateKey:        w.PrivateKeyStr(),
+		PublicKey:         w.PublicKeyStr(),
+		BlockchainAddress: w.BlockchainAddress(),
+	})
+}
+
 type Transaction struct {
 	senderPrivateKey          *ecdsa.PrivateKey
 	senderPublicKey           *ecdsa.PublicKey
