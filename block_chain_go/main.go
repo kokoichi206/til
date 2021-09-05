35205fca632451c525233793df1852d998d938bc
kokoichi206 <tt.300607@gmail.com>
2021-09-05 14:47:51 +0900

    feat: Signature

--- block_chain_go/main.go
+++ block_chain_go/main.go
@@ -15,4 +15,8 @@ func main() {
 	fmt.Println(w.PrivateKeyStr())
 	fmt.Println(w.PublicKey())
 	fmt.Println(w.BlockchainAddress())
+
+	// トランザクションのストラクチャーを作る
+	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	fmt.Printf("signature %s\n", t.GenerateSignature())
 }
