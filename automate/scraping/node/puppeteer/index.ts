23aa39d54678efa95251dec2bd73cd9aed4cee89
takahiro.tominaga <tt.300607@gmail.com>
2022-10-05 02:53:22 +0900

    メンバー詳細情報を取得

--- automate/scraping/node/puppeteer/index.ts
+++ automate/scraping/node/puppeteer/index.ts
@@ -1,40 +1,12 @@
 import Puppetter from "puppeteer";
 
-const launchOptions = {
-  headless: true,
-  slowMo: 100,
-  defaultViewport: {
-    width: 1700,
-    height: 1000
-  },
-  args: [
-    '--lang=ja',
-    '--no-sandbox'
-  ],
-}
+import crawler, { Member } from './crawler';
 
 async function main() {
-  const browser = await Puppetter.launch(launchOptions);
-  const url = "https://pptr.dev/";
+  const browser = await Puppetter.launch(crawler.launchOptions());
 
-  const page = await browser.newPage();
-  await setupPage(page);
-  await page.goto(url);
-
-  const selector = ".anchor";
-  const links = await page.$$eval(selector, el => {
-    return el.map(e => e.textContent);
-  });
-  console.log(links);
+  const members = await crawler.crawl(browser);
   browser.close();
-}
-
-async function setupPage(page: Puppetter.Page): Promise<void> {
-  await page.setExtraHTTPHeaders({
-    "Accept-Language": "ja-JP"
-  });
-
-  await page.setCacheEnabled(false);
 }
 
 main()
