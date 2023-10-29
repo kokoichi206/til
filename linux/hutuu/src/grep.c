cba97c66ee3603bfc005b9d66d469ca540df3bc7
takahiro.tominaga <tt.300607@gmail.com>
2023-10-29 13:56:41 +0900

    sec 8 add ignore-case feat to grep

--- linux/hutuu/src/grep.c
+++ linux/hutuu/src/grep.c
@@ -3,22 +3,64 @@
 #include <string.h>
 #include <sys/types.h>
 #include <regex.h>
+#define _GNU_SOURCE
+#include <getopt.h>
+#include <stdarg.h>
 
 static void do_grep(regex_t *pat, FILE *f);
+static void debug_print(const char *fmt, ...);
 
-int main(int argc, char const *argv[])
+static int opt_ignorecase = 0;
+static int debug = 0;
+
+static struct option longopts[] = {
+    {"ignore-case", no_argument, NULL, 'i'},
+    {NULL, 0, NULL, 0},
+};
+
+int main(int argc, char *argv[])
 {
     regex_t pat;
     int err;
     int i;
 
-    if (argc < 2)
+    int opt;
+
+    while ((opt = getopt_long(argc, argv, "id", longopts, NULL)) != -1)
+    {
+        switch (opt)
+        {
+        case 'i':
+            opt_ignorecase = 1;
+            break;
+        case 'd':
+            debug = 1;
+            break;
+        case '?':
+            exit(1);
+        }
+    }
+
+    argc -= optind;
+    argv += optind;
+
+    debug_print("optind %d\n", optind);
+    debug_print("argc %d\n", argc);
+
+    if (argc < 1)
     {
         fputs("no pattern\n", stderr);
         exit(1);
     }
 
-    err = regcomp(&pat, argv[1], REG_EXTENDED | REG_NOSUB | REG_NEWLINE);
+    int re_mode = REG_EXTENDED | REG_NOSUB | REG_NEWLINE;
+    if (opt_ignorecase)
+        re_mode |= REG_ICASE;
+
+    debug_print("argv[0] %s\n", argv[0]);
+    debug_print("argv[1] %s\n", argv[1]);
+
+    err = regcomp(&pat, argv[0], re_mode);
     if (err != 0)
     {
         char buf[1024];
@@ -28,13 +70,13 @@ int main(int argc, char const *argv[])
         exit(1);
     }
 
-    if (argc == 2)
+    if (argc == 1)
     {
         do_grep(&pat, stdin);
     }
     else
     {
-        for (i = 2; i < argc; i++)
+        for (i = 1; i < argc; i++)
         {
             FILE *f;
 
@@ -55,8 +97,22 @@ int main(int argc, char const *argv[])
 }
 
 static void
+debug_print(const char *fmt, ...)
+{
+    if (debug)
+    {
+        va_list args;
+        va_start(args, fmt);
+        vprintf(fmt, args);
+        va_end(args);
+    }
+}
+
+static void
 do_grep(regex_t *pat, FILE *src)
 {
+    debug_print("%s", "do_grep");
+
     char buf[4096];
 
     while (fgets(buf, sizeof buf, src))
