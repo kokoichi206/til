73330b4d8cb39bc6dfaf1a9768ad6b5c2ab5d99c
kokoichi206 <tt.300607@gmail.com>
2024-04-06 18:42:46 +0900

    filter projects

--- laravel/laravel11-react/resources/js/Pages/Project/Index.jsx
+++ laravel/laravel11-react/resources/js/Pages/Project/Index.jsx
@@ -1,9 +1,29 @@
 import Pagination from "@/Components/Pagination";
+import SelectInput from "@/Components/SelectInput";
+import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
 
-export default function Index({ auth, projects }) {
+export default function Index({ auth, projects, queryParams = null }) {
+    queryParams = queryParams || {};
+
+    const searchFieldChanged = (name, value) => {
+        if (value) {
+            queryParams[name] = value;
+        } else {
+            delete queryParams[name];
+        }
+
+        router.get(route("project.index"), queryParams);
+    };
+
+    const onKeyPress = (name, e) => {
+        if (e.key === "Enter") {
+            searchFieldChanged(name, e.target.value);
+        }
+    };
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -38,6 +58,61 @@ export default function Index({ auth, projects }) {
                                         </th>
                                     </tr>
                                 </thead>
+
+                                <thead className="text-xs text-gray-100 uppercase bg-gray-50 dark:bg-gray-700 border-gray-500">
+                                    <tr className="text-nowrap">
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2">
+                                            <TextInput
+                                                className="w-full"
+                                                defaultValue={queryParams.name}
+                                                placeholder="Project Name"
+                                                onBlur={(e) =>
+                                                    searchFieldChanged(
+                                                        "name",
+                                                        e.target.value
+                                                    )
+                                                }
+                                                onKeyPress={(e) =>
+                                                    onKeyPress("name", e)
+                                                }
+                                            />
+                                        </th>
+                                        <th className="px-3 py-2">
+                                            <SelectInput
+                                                className="w-full"
+                                                defaultValue={
+                                                    queryParams.status
+                                                }
+                                                onChange={(e) =>
+                                                    searchFieldChanged(
+                                                        "status",
+                                                        e.target.value
+                                                    )
+                                                }
+                                            >
+                                                <option value="">
+                                                    Select Status
+                                                </option>
+                                                <option value="pending">
+                                                    Pending
+                                                </option>
+                                                <option value="in_progress">
+                                                    In Progress
+                                                </option>
+                                                <option value="completed">
+                                                    Completed
+                                                </option>
+                                            </SelectInput>
+                                        </th>
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2"></th>
+                                        <th className="px-3 py-2"></th>
+                                    </tr>
+                                </thead>
+
                                 <tbody>
                                     {projects.data.map((project) => (
                                         <tr
