1a58a475d5262f65b5b8b749720ae1070ae57ccf
kokoichi206 <tt.300607@gmail.com>
2024-04-07 00:21:59 +0900

    Create Project

--- laravel/laravel11-react/app/Http/Controllers/ProjectController.php
+++ laravel/laravel11-react/app/Http/Controllers/ProjectController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\UpdateProjectRequest;
 use App\Http\Resources\ProjectResource;
 use App\Http\Resources\TaskResource;
 use App\Models\Project;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class ProjectController extends Controller
 {
@@ -28,10 +30,12 @@ class ProjectController extends Controller
         }
 
         $projects = $query->orderBy($sortField, $sortDirection)->paginate(10)->onEachSide(1);
+        // dd($projects);
 
         return inertia('Project/Index', [
             "projects" => ProjectResource::collection($projects),
             'queryParams' => request()->query() ?: null,
+            'success' => session('success'),
         ]);
     }
 
@@ -40,7 +44,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return inertia('Project/Create');
     }
 
     /**
@@ -48,7 +52,21 @@ class ProjectController extends Controller
      */
     public function store(StoreProjectRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data['created_by'] = Auth::id();
+        $data['updated_by'] = Auth::id();
+
+        // image is UploadedFile instance
+        /** @var $image \Illuminate\Http\UploadedFile */
+        $image = $data['image'] ?? null;
+        if ($image) {
+            // storage/app/public/project/abc.jpg
+            $data['image_path'] = $image->store('project/' . Str::random(), 'public');
+        }
+
+        Project::create($data);
+
+        return to_route('project.index')->with('success', 'Project was created successfully!');
     }
 
     /**
--- laravel/laravel11-react/resources/js/Pages/Project/Index.jsx
+++ laravel/laravel11-react/resources/js/Pages/Project/Index.jsx
@@ -6,7 +6,7 @@ import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants";
 import { Head, Link, router } from "@inertiajs/react";
 import TableHeading from "@/Components/TableHeading";
 
-export default function Index({ auth, projects, queryParams = null }) {
+export default function Index({ auth, projects, queryParams = null, success }) {
     queryParams = queryParams || {};
 
     const searchFieldChanged = (name, value) => {
@@ -45,15 +45,28 @@ export default function Index({ auth, projects, queryParams = null }) {
         <AuthenticatedLayout
             user={auth.user}
             header={
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Projects
-                </h2>
+                <div className="flex justify-between items-center">
+                    <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                        Projects
+                    </h2>
+                    <Link
+                        href={route("project.create")}
+                        className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+                    >
+                        Add new
+                    </Link>
+                </div>
             }
         >
             <Head title="Dashboard" />
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    {success && (
+                        <div className="bg-emerald-500 py-2 px-4 mb-4 text-white">
+                            {success}
+                        </div>
+                    )}
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
