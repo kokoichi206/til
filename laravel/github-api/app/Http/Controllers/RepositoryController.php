73b5eb5899aa25a77e34cbf867de9caea820e4b5
kokoichi206 <tt.300607@gmail.com>
2024-04-07 19:15:11 +0900

    github api を呼び出しローカルに保存する

--- laravel/github-api/app/Http/Controllers/RepositoryController.php
+++ laravel/github-api/app/Http/Controllers/RepositoryController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRepositoryRequest;
 use App\Http\Resources\RepositoriesResource;
 use App\Models\Repository;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class RepositoryController extends Controller
 {
+    private $base = "https://api.github.com";
+
     public function getRepositories()
     {
         $repositories = Repository::all();
@@ -15,7 +21,37 @@ class RepositoryController extends Controller
         $res['repositories'] = $repositories;
         $res['total'] = $total;
 
-        // dd($res);
         return RepositoriesResource::make($res);
+    }
+
+    public function registerRepositories(RegisterRepositoryRequest $request)
+    {
+        $page = $request->page ?? 1;
+        $res = Http::get("{$this->base}/users/{$request->name}/repos?page={$page}");
+        if ($res->status() !== 200) {
+            Log::error("github api error", ["status" => $res->status(), "body" => $res->body()]);
+            return response()->json(['message' => 'ERROR'], $res->status());
+        }
+
+        $bulk = [];
+        $repos = $res->json();
+        for ($i = 0; $i < count($repos); $i++) {
+            $repository = $repos[$i];
+            $bulk[] = [
+                'id' => Str::uuid(),
+                'name' => $repository['name'],
+                'description' => $repository['description'] ?? null,
+                'license' => $repository['license']['spdx_id'] ?? null,
+                'open_issues' => $repository['open_issues'],
+                'gh_user_name' => $request->name,
+                'fork' => $repository['fork'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+        }
+
+        Repository::insert($bulk);
+
+        return response()->noContent(204);
     }
 }
