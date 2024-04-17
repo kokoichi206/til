2add0601f1a16815a66d7db2c53c98ff9548c99f
kokoichi206 <tt.300607@gmail.com>
2024-04-17 23:32:38 +0900

    chore

--- laravel/lighthouse/database/seeders/DatabaseSeeder.php
+++ laravel/lighthouse/database/seeders/DatabaseSeeder.php
@@ -6,7 +6,6 @@ use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
