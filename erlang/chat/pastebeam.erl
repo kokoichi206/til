f1bad657d834a84d4ca255b2f1a6e847a937fffc
TAKAHiRO TOMiNAGA <52474650+kokoichi206@users.noreply.github.com>
2026-03-22 07:56:07 +0900

    erlang challenge state

--- erlang/chat/pastebeam.erl
+++ erlang/chat/pastebeam.erl
@@ -26,8 +26,17 @@ start() ->
 %             server(NewMessage)
 %     end.
 
+fail_session(Sock, Reason) ->
+    io:format("ERROR: session failed: ~w\n", [Reason]),
+    gen_tcp:close(Sock),
+    ok.
+
 -spec session(State, Sock) -> ok when
-      State :: command | {challenge, list(binary())} | {post, list(binary())} | {get, unicode:chardata()},
+      State :: command |
+                {challenge, list(binary())} |
+                {accepted, list(binary()), binary()} |
+                {post, list(binary())} |
+                {get, unicode:chardata()},
       Sock :: gen_tcp:socket().
 session(command, Sock) ->
     case gen_tcp:recv(Sock, 0) of
@@ -40,9 +49,7 @@ session(command, Sock) ->
             gen_tcp:close(Sock),
             ok;
         {error, Reason} ->
-            io:format("ERROR: session failed: ~w\n", [Reason]),
-            gen_tcp:close(Sock),
-            ok
+            fail_session(Sock, Reason)
     end;
 session({post, Lines}, Sock) ->
     case gen_tcp:recv(Sock, 0) of
@@ -51,16 +58,24 @@ session({post, Lines}, Sock) ->
         {ok, Line} ->
             session({post, [Line|Lines]}, Sock);
         {error, Reason} ->
-            io:format("ERROR: session failed: ~w\n", [Reason]),
+            fail_session(Sock, Reason)
+    end;
+session({challenge, Lines}, Sock) ->
+    Challenge = binary:encode_hex(crypto:strong_rand_bytes(32)),
+    gen_tcp:send(Sock, [<<"CHALLENGE: ">>, Challenge, <<"\r\n">>]),
+    session({accepted, Lines, Challenge}, Sock);
+session({accepted, Lines, Challenge}, Sock) ->
+    case gen_tcp:recv(Sock, 0) of
+        {ok, <<"ACCEPTED ", Id/binary>>} -> 'TODO';
+        {ok, _} -> 
+            gen_tcp:send(Sock, "INVALID COMMAND\r\n"),
             gen_tcp:close(Sock),
-            ok
+            ok;
+        {error, Reason} ->
+            fail_session(Sock, Reason)
     end;
 session({get, Id}, Sock) ->
     io:format("TODO: GET MODE~s\n", [Id]),
-    gen_tcp:close(Sock),
-    ok;
-session({challenge, Lines}, Sock) ->
-    io:format("TODO: CHALLENGE: ~w\n", [Lines]),
     gen_tcp:close(Sock),
     ok.
 
